997d74a5d4ddf92fc9c82634dd256d7bf4779ce2
practiceuserisa <53205564+practiceuserisa@users.noreply.github.com>
2019-07-23 09:56:48 +0530

    First Github Program
    First Github Program

---
+++ helloGithub.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class helloGithub
+{
+	static boolean gitHub()
+	{
+		return true;
+	}
+	
+	public static void main(String[] args)
+	{
+		if(gitHub())
+		{
+			System.out.println("Hello GitHub");
+		}
+	}
+}
